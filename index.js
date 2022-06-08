df3c9897aa983c05575aade554d916c7309c5d24
chocomelo24 <mansoorchanique@gmail.com>
2022-06-08 14:58:38 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let mainListDiv = document.getElementById("mainListDiv"),
+  mediaButton = document.getElementById("mediaButton");
+mediaButton.onclick = function () {
+  "use strict";
+  mainListDiv.classList.toggle("show_list");
+  mediaButton.classList.toggle("active");
+};
